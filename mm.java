50c2d8904c4f0f96c06697431fe4e320f2282093
farel mufadal <56870694+farelmufadal08@users.noreply.github.com>
2019-10-23 02:44:55 +0700

    Add files via upload
    Tugas stack

---
+++ mm.java
@@ -0,0 +1,39 @@
+public class mm {
+    public static void main(String[] args) {
+
+            cobak x = new cobak (13);
+            x.push ("a");
+            x.push ("t");
+            x.push ("a");
+            x.push ("d");
+            x.push (" ");
+            x.push ("r");
+            x.push ("u");
+            x.push ("t");
+            x.push ("k");
+            x.push ("u");
+            x.push ("r");
+            x.push ("t");
+            x.push ("s");
+
+
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+            System.out.print(x.pop());
+
+
+
+
+
+    }
+}
